dff309d8c65032158b692b62ca8ae8cdabe10e36
Sumitdhameliya11 <sumitdhameliya02@gmail.com>
2024-09-11 21:00:49 +0530

    tokenerrorsolve

--- src/api/Axiosinstance.js
+++ src/api/Axiosinstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
-const token  = Cookies.get("token");
+// const token  = Cookies.get("token");
 const AxiosInstance  = axios.create({
     // baseURL:'http://localhost:4000/',
     baseURL:'https://cms-node-f3gn.onrender.com/',
     headers: {
-        authorization: `Bearer  ${token}`
+        authorization: `Bearer  ${Cookies.get("token")}`
     },
 })
 
