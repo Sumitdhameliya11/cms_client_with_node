020610ab8545525826d7599ba68da658a28a604e
imparth7 <imparthdudhatra@gmail.com>
2024-09-18 23:04:57 +0530

    Team_Section_Rebuild

--- src/component/Team.js
+++ src/component/Team.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 import sumit from '../image/sumit.png';
 import nirav from '../image/nirav.jpg';
 import parth from '../image/parth.jpg';
@@ -8,85 +9,119 @@ import astha from '../image/astha.jpg';
 import '../css/team.css';
 
 const developers = [
-    {
-      id: 1,
-      name: "Sumit Dhameliya",
-      image: sumit,
-      specialization: "Full-stack Developer",
-      skill: "React.js, MySQL, MongoDB, Node.js, PHP, Bootstrap",
-      instagram: "https://instagram.com/sumit_dhameliya",
-    },
-    {
-      id: 2,
-      name: "Nirav Mathukiya",
-      image: nirav,
-      specialization: "Full-stack Developer",
-      skill: "React.js, Next.js, Node.js, Bootstrap, Tailwind, JavaScript",
-      instagram: "https://instagram.com/nirav_mathukiya",
-    },
-    {
-      id: 3,
-      name: "Parth Dudhatra",
-      image: parth,
-      specialization: "Front-End Developer",
-      skill: "javascript ,boostrap, tailwind CSS ,react.js",
-      instagram: "https://instagram.com/parth_dudhatra",
-    },
-    {
-      id: 4,
-      name: "Uttam Dobariya",
-      image: uttam,
-      specialization: "Front-End Developer",
-      skill: "React.js, Next.js, Tailwind CSS, Bootstrap",
-      instagram: "https://instagram.com/uttam_dobariya",
-    },
-    {
-      id: 5,
-      name: "Shruti Ghevariya",
-      image: shruti,
-      specialization: "Tester",
-      skill: "Postman, Documentation",
-      instagram: "https://instagram.com/shruti_ghevariya",
-    },
-    {
-      id: 6,
-      name: "Astha Maniya",
-      image: astha,
-      specialization: "Tester",
-      skill: "Postman, Documentation",
-      instagram: "https://instagram.com/astha_maniya",
-    },
+  {
+    id: 1,
+    name: "Sumit Dhameliya",
+    image: sumit,
+    specialization: "Full-stack Developer",
+    skill: "MongoDB, Express, React.js, Node.js, MYSql, PHP, Bootstrap",
+    github: "https://github.com/sumit_dhameliya",
+  },
+  {
+    id: 2,
+    name: "Nirav Mathukiya",
+    image: nirav,
+    specialization: "Full-stack Developer",
+    skill: "React.js, Next.js, Node.js, Bootstrap, Bootstrap, JavaScript",
+    github: "https://github.com/nirav_mathukiya",
+  },
+  {
+    id: 3,
+    name: "Parth Dudhatra",
+    image: parth,
+    specialization: "Front-End Developer",
+    skill: "React.js, Next.js, React Native, Expo, JavaScript, TypeScript, Tailwind CSS, SASS, UI Libraries",
+    github: "https://github.com/imparth7",
+  },
+  {
+    id: 4,
+    name: "Uttam Dobariya",
+    image: uttam,
+    specialization: "Front-End Developer",
+    skill: "React.js, React Native, Tailwind CSS, Bootstrap, JavaScript, Chakra UI",
+    github: "https://github.com/uttam_dobariya",
+  },
+  {
+    id: 5,
+    name: "Shruti Ghevariya",
+    image: shruti,
+    specialization: "Tester",
+    skill: "Postman, Documentation",
+    github: "https://github.com/shruti_ghevariya",
+  },
+  {
+    id: 6,
+    name: "Astha Maniya",
+    image: astha,
+    specialization: "Tester",
+    skill: "Postman, Documentation",
+    github: "https://github.com/astha_maniya",
+  },
 ];
 
 function Team() {
-    return (
-        <div className="container my-5">
-            <h2 className="text-center mb-4">Our Developers</h2>
-            <div className="row">
-                {developers.map((developer) => (
-                    <div className="col-12 col-md-6 col-lg-4 mb-4" key={developer.id}>
-                        <div className="card h-100 d-flex flex-column shadow-sm">
-                            <img
-                                src={developer.image}
-                                className="card-img-top"
-                                alt={developer.name}
-                                style={{ objectFit: 'cover', height: '300px' }} // Increased image height
-                            />
-                            <div className="card-body d-flex flex-column">
-                                <h5 className="card-title">{developer.name}</h5>
-                                <p className="card-text">
-                                    <strong>Specialization:</strong> {developer.specialization}
-                                </p>
-                                <p className="card-text mt-auto">
-                                    <strong>Skills:</strong> {developer.skill}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+  const [selectedDeveloper, setSelectedDeveloper] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCardClick = (developer) => {
+    setSelectedDeveloper(developer);
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    setSelectedDeveloper(null);
+  };
+
+  return (
+    <div className="container my-5">
+      <h2 className="text-center mb-4">Our Developers</h2>
+      <div className="row">
+        {developers.map((developer) => (
+          <div className="col-12 col-md-6 col-lg-4 mb-4" key={developer.id}>
+            <div className="card h-100 d-flex flex-column shadow-sm" onClick={() => handleCardClick(developer)}  style={{ borderRadius: 10, overflow: 'hidden' }}>
+              <img
+                src={developer.image}
+                className="card-img-top"
+                alt={developer.name}
+                style={{ objectFit: 'cover', height: '300px' }}
+              />
+              <div className="card-body text-center" style={{ position: 'absolute', bottom: 0, background: '#FFFA', width: "94%", margin: "3.5% 3%", borderRadius: 15 }}>
+                <h5 className="card-title">{developer.name}</h5>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+
+      {selectedDeveloper && (
+        <Modal show={showModal} onHide={handleClose} dialogClassName="modal-dialog-centered" style={{ zIndex: 999999999999 }}>
+          <Modal.Header closeButton>
+            <Modal.Title>{selectedDeveloper.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img
+              src={selectedDeveloper.image}
+              className="img-fluid mb-3"
+              alt={selectedDeveloper.name}
+              style={{
+                objectFit: 'cover', width: '100%', aspectRatio: "4/3", borderRadius: "25px",
+                // marginLeft: "25%"
+              }}
+            />
+            <h4><strong>{selectedDeveloper.specialization}</strong></h4>
+            <p>{selectedDeveloper.skill}</p>
+            <p><a href={selectedDeveloper.github} target="_blank" className='btn btn-primary'>Github</a></p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
+  );
 }
 
 export default Team;
